063ab6976b09836aebd51618ead9c039321f5b9e
Charles Luzar <mail@charlesluzar.com>
2025-02-28 16:39:42 -0500

    added code blocks for javascript

--- app/components/code_block.rb
+++ app/components/code_block.rb
@@ -1,7 +1,11 @@
 class RelayUi::CodeBlock < RelayUi::Base
+  def initialize(language: "ruby")
+    @language = language
+  end
+
   def view_template
     pre do
-      code(class: "language-ruby rounded-lg") do
+      code(class: "language-#{@language} rounded-lg") do
         yield
       end
     end
--- docs/app/views/components/code_blocks_view.rb
+++ docs/app/views/components/code_blocks_view.rb
@@ -6,7 +6,7 @@ class Views::Components::CodeBlocksView < Views::Base
         "This section demonstrates how to render a code block in RelayUI."
       end
 
-      render Components::Description.new("Ruby") do
+      render Components::Description.new("Ruby (Default)") do
         render RelayUi::CodeBlock.new do
         <<-RUBY
 def sum_eq_n?(arr, n)
@@ -26,6 +26,30 @@ end
   \RUBY
 end
         RUBY
+        end
+      end
+
+      render Components::Description.new("Javascript (Override)") do
+        render RelayUi::CodeBlock.new(language: "javascript") do
+          <<-JS
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+          JS
+        end
+      render RelayUi::CodeBlock.new do
+        <<-JS
+render RelayUi::CodeBlock.new(language: "javascript") do
+  \<<-JS
+def sum_eq_n?(arr, n)
+  return true if arr.empty? && n == 0
+  arr.product(arr).reject { |a,b| a == b }.any? { |a,b| a + b == n }
+end
+  \JS
+end
+        JS
         end
       end
     end

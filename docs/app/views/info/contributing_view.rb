f541be206901553854c6f905cb2a96a1351854e2
Charles Luzar <mail@charlesluzar.com>
2025-03-11 23:13:03 -0400

    updates

--- docs/app/views/info/contributing_view.rb
+++ docs/app/views/info/contributing_view.rb
@@ -1,17 +1,33 @@
 class Views::Info::ContributingView < Views::Base
   def view_template
     render Views::Layouts::ApplicationLayout.new do
-      div class: "flex flex-col gap-8" do
-        render RUI::Headings::H1.new { "Contributing" }
-        render RUI::Text::Markdown.new do
-<<-MARKDOWN
-If you're interested in contributing on this project we'd love to chat with you on [our Discord server](https://discord.gg/wPyvK87KFe).
+      render RUI::Markdown.new(html_safe: false) do
+<<-STRING
+# Contributing
 
-For more information on contributing to this gem, please see the README file on [GitHub](https://www.github.com/logicrelay/relay_ui).
+**If you're interested in contributing on this project we'd love to chat with you on [our Discord server](https://discord.gg/wPyvK87KFe).**
 
-Bug reports and pull requests are [welcome on GitHub](https://github.com/logicrelay/relay_ui). This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [code of conduct](https://github.com/logicrelay/relay_ui/blob/main/CODE_OF_CONDUCT.md).
-MARKDOWN
-        end
+There's a lot of work to be done on this project, and we're always looking for help. Here's some details on how we're going about building this kit:
+
+- We have a [public roadmap](https://github.com/orgs/logicrelay/projects/14/views/3) that we're using to track features that are slated for the next release and features that haven't been prioritized yet.
+- We're using [Github Projects](https://github.com/orgs/logicrelay/projects/14/views/1) to track WIP features and bugs.
+- We'll generally spec out a milestone in a JIT fashion as the previous milestone is released.
+
+## Prerequisites
+
+There are a couple of things you'll need to do to get your environment set up for contributing to this project:
+
+- **mise** - We use [`mise`](https://mise.jdx.dev/getting-started.html) to manage our development environment and provide easy access to the tools we need to work on the project.
+- **bundler config** - You'll need to set the local path to the root directory of the project by running the following command in the root of the repo: `bundle config local.relay_ui /path/to/relay_ui`. This will allow you to work on the UI kit locally and see changes in real time.
+- **LiveReload** - [LiveReload](https://addons.mozilla.org/en-US/firefox/addon/livereload/) is not a must have, but it's a nice to have. It will automatically refresh your browser when you're working on the UI kit locally.
+
+## Tooling
+
+There are three processes to run locally when developing...
+- `mise run watch` starts the watch process that generates a combined CSS file using TailwindCSS.
+- `mise run server` starts the local server.
+- `mise run guard` runs `guard` and `guard-livereload` (optional)
+STRING
       end
     end
   end
--- docs/app/views/info/installation_view.rb
+++ docs/app/views/info/installation_view.rb
@@ -1,39 +1,36 @@
 class Views::Info::InstallationView < Views::Base
   def view_template
     render Views::Layouts::ApplicationLayout.new do
-      div class: "flex flex-col gap-8" do
-        render RUI::Headings::H1.new { "Installation" }
-        render RUI::Text::Medium.new do
-          "Add this line to your application's Gemfile:"
-        end
-        render RUI::CodeBlock.new do
-<<-RUBY
+      render RUI::Markdown.new(html_safe: false) do
+<<-STRING
+# Installation
+
+### First, add the gem to your `Gemfile`:
+
+```ruby
 gem 'relay_ui'
-RUBY
-        end
-        render RUI::Text::Medium.new { "And then execute:" }
-        render RUI::CodeBlock.new do
-<<-RUBY
+```
+
+### Then install your gems:
+
+```shell
 bundle install
-RUBY
-        end
-        render RUI::Text::Medium.new { "Or install it directly:" }
-        render RUI::CodeBlock.new do
-<<-RUBY
+```
+
+### Or install it directly:
+
+```shell
 gem install relay_ui
-RUBY
-        end
-        render RUI::Text::Medium.new do
-          "Include the gem's stylesheet in your application layout:"
-        end
-        render RUI::CodeBlock.new do
-<<-RUBY
+```
+
+### Include the gem's stylesheet in your application layout:
+
+```ruby
 stylesheet_link_tag "relay_ui/relay_ui", media: "all"
-RUBY
-        end
-        render RUI::Text::Medium.new do
-          "That's it! All of the basic functionality of the UI kit is now available to you. For certain components that require additional stylesheets and stimulus controllers, you'll need to include those separately. They will be documented in the component's usage instructions."
-        end
+```
+
+That's it! All of the basic functionality of the UI kit is now available to you. For certain components that require additional elements (like stimulus controllers), you'll need to include those separately. They will be documented in the component's usage instructions.
+STRING
       end
     end
   end

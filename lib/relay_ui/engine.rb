e9a673c65972b814de81aab0798a87078138f99b
Charles Luzar <mail@charlesluzar.com>
2025-03-19 09:20:57 -0400

    ditch dist dir

--- lib/relay_ui/engine.rb
+++ lib/relay_ui/engine.rb
@@ -5,10 +5,6 @@ module RUI
     class Engine < ::Rails::Engine
       isolate_namespace RUI
 
-      initializer "relay_ui.assets.precompile" do |app|
-        app.config.assets.paths << root.join("dist")
-      end
-
       initializer "relay_ui.autoload.components" do
         Rails.autoloaders.main.push_dir(
           "#{Gem::Specification.find_by_name('relay_ui').gem_dir}/lib/rui",

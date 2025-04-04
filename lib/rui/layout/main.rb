1c40240006eed96f4c73e85877239b3ec962a0cc
Charles Luzar <mail@charlesluzar.com>
2025-04-04 12:01:08 -0400

    sticky side nav

--- lib/rui/layout/main.rb
+++ lib/rui/layout/main.rb
@@ -1,5 +1,5 @@
 class RUI::Layout::Main < RUI::Base
   def view_template(&)
-    main(class: "p-5 lg:p-10 w-full max-w-[800px]", &)
+    main(class: "p-5 lg:p-10 lg:ml-72 w-full max-w-[800px]", &)
   end
 end
--- lib/rui/navigation/sidebar.rb
+++ lib/rui/navigation/sidebar.rb
@@ -2,7 +2,7 @@
 
 class RUI::Navigation::Sidebar < RUI::Base
   def view_template
-    div(class: "p-10 pb-36 hidden lg:flex flex-col bg-white z-50 lg:z-auto fixed lg:relative w-screen md:w-auto h-screen lg:h-auto overflow-auto", data: { "navigation-target": "sidebar" }) do
+    div(class: "lg:w-72 p-10 pb-36 hidden lg:flex flex-col bg-white z-50 lg:z-auto fixed w-screen md:w-auto h-screen lg:max-h-full overflow-y-auto", data: { "navigation-target": "sidebar" }) do
       yield
     end
     button(class: "lg:hidden hidden bg-black/75 fixed w-screen h-screen", data: { navigation_target: "curtain", action: "navigation#toggle" })
